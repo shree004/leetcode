fc0985855af273f96e8eef764b5bc89254a8952b
Shree Varsaan <98572720+shree004@users.noreply.github.com>
2024-09-14 12:07:02 +0530

    Time: 1 ms (78.55%) | Memory: 45.4 MB (70.64%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,14 +1,4 @@
 class Solution {
-    public void markcolumn(int[][] nums,int n){
-        for(int i=0;i<nums.length;i++){
-            nums[i][n]=0;
-        }
-    }
-    public void markrow(int[][] nums,int m){
-        for(int i=0;i<nums[0].length;i++){
-            nums[m][i]=0;
-        }
-    }
     public void setZeroes(int[][] nums) {
         int[] col=new int[nums[0].length];
         int[] row=new int[nums.length];
@@ -21,10 +11,11 @@ class Solution {
             }
         }
         for(int i=0;i<nums.length;i++){
-            if(row[i]==-1) markrow(nums,i);
-        }
-        for(int j=0;j<nums[0].length;j++){
-            if(col[j]==-1) markcolumn(nums,j);
+            for(int j=0;j<nums[0].length;j++){
+                if(row[i]==-1 || col[j]==-1){
+                    nums[i][j]=0;
+                }
+            }
         }
     }
 
