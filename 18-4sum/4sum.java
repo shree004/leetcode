728854f62ef2370946e8b1943be5b99734592faf
Shree Varsaan <98572720+shree004@users.noreply.github.com>
2024-09-14 18:48:57 +0530

    Time: 19 ms (44.03%) | Memory: 44.1 MB (45.71%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -3,8 +3,10 @@ class Solution {
         Arrays.sort(nums);
         Set<List<Integer>> list=new HashSet<>();
         for(int i=0;i<nums.length-3;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int start=nums[i];
             for(int end=nums.length-1;end>i+2;end--){
+                if(end<(nums.length-1) && nums[end] == nums[end+1]) continue;
                 int left=i+1;
                 int right=end-1;
                 while(left<right){
@@ -20,6 +22,8 @@ class Solution {
                     }
                     left++;
                     right--;
+                    while(left<right && nums[left]==nums[left-1]) left++;
+                    while(left<right && nums[right]==nums[right+1]) right--;
                 }
                 else if(sum<target) left++;
                 else right--;
