8ed2a67bcab972affb9641724c9cd97194cc7063
Shree Varsaan <98572720+shree004@users.noreply.github.com>
2024-09-23 10:46:50 +0530

    Time: 25 ms (49.65%) | Memory: 44.8 MB (78.37%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,11 +1,4 @@
 class Solution {
-    public int max(int[]arr){
-        int max=Integer.MIN_VALUE;
-        for(int i:arr){
-            if(max<i) max=i;
-        }
-        return max;
-    }
     public int noOfHours(int n,int[] arr){
         int count=0;
         for(int i:arr) count+=Math.ceil((double)i/(double)n);
@@ -13,7 +6,7 @@ class Solution {
     }
     public int minEatingSpeed(int[] piles, int h) {
         int low=0;
-        int high=max(piles);
+        int high=1000000000;
         while(low<=high){
             int mid=(low+high)/2;
             if(noOfHours(mid,piles)<=h){
