e3eb226cff85caa5d1d64bb06b2acb59502a2b8f
Shree Varsaan <98572720+shree004@users.noreply.github.com>
2024-09-24 21:55:58 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (5.49%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,37 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n=nums1.length+nums2.length;
-        int i=0,j=0;
-        int cnt=0;
-        int ele1=0,ele2=0;
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i]<=nums2[j]){
-                if(cnt==n/2) ele1=nums1[i];
-                if(cnt==(n/2-1)) ele2=nums1[i];
-                i++;cnt++;
+        int n1=nums1.length;
+        int n2=nums2.length;
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int low=0;
+        int high=n1;
+        int n=n1+n2;
+        int left=(n1+n2+1)/2;
+        while(low<=high){
+            int mid1=(low+high)/2;
+            int mid2=left-mid1;
+            int l1=Integer.MIN_VALUE,l2=Integer.MIN_VALUE;
+            int r1=Integer.MAX_VALUE,r2=Integer.MAX_VALUE;
+            if(mid1-1>=0) l1=nums1[mid1-1];
+            if(mid2-1>=0) l2=nums2[mid2-1];
+            if(mid1<n1) r1=nums1[mid1];
+            if(mid2<n2) r2=nums2[mid2];
+            if(l1<=r2 && l2<=r1){
+                if(n%2==1) return Math.max(l1,l2);
+                else{
+                    return (double)(Math.max(l1,l2)+Math.min(r1,r2))/2;
+                }
             }
-            else{
-                if(cnt==n/2) ele1=nums2[j];
-                if(cnt==(n/2-1)) ele2=nums2[j];
-                j++;cnt++;
-            }
+            else if(l1>r2) high=mid1-1;
+            else if(l2>r1) low=mid1+1;
         }
-        while(i<nums1.length){
-            if(cnt==n/2) ele1=nums1[i];
-                if(cnt==(n/2-1)) ele2=nums1[i];
-                i++;cnt++;
-        }
-        while(j<nums2.length){
-            if(cnt==n/2) ele1=nums2[j];
-                if(cnt==(n/2-1)) ele2=nums2[j];
-                j++;cnt++;
-        }
-        
-        if(n%2==0){
-            return ((double)ele1+(double)ele2)/2.0;
-        }
-        else{
-            return ele1;
-        }
+        return -1;
     }
 }
