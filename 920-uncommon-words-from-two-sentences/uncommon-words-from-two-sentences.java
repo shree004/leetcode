b11970373131cc6451aa3891a58bebe530cb62ee
Shree Varsaan <98572720+shree004@users.noreply.github.com>
2024-09-27 22:17:42 +0530

    Time: 6 ms (14.89%) | Memory: 42.1 MB (48.98%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
@@ -7,10 +7,10 @@ class Solution {
         for(String i:str1){
             map.put(i,map.getOrDefault(i,0)+1);
         }
-        Iterator<Map.Entry<String,Integer>> it=map.entrySet().iterator();
-        while(it.hasNext()){
-            Map.Entry<String,Integer> entry=it.next();
-            if(entry.getValue()==1) arr.add(entry.getKey());
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                arr.add(entry.getKey());
+            }
         }
         return arr.toArray(new String[0]);
     }
