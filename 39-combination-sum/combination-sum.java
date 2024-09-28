4f72c2e72982300a6c1f2ef5f8a7d285a9aac244
Shree Varsaan <98572720+shree004@users.noreply.github.com>
2024-09-28 14:04:45 +0530

    Time: 2 ms (83.35%) | Memory: 44.5 MB (68.75%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -5,17 +5,14 @@ class Solution {
         return arr;
     }
     public void helper(int[]candidates,int index,int sum, List<Integer> list,List<List<Integer>> arr){
-        if(index==candidates.length){
-            if(sum==0){
-                arr.add(new ArrayList<>(list));
-            }
+        if(sum==0){
+            arr.add(new ArrayList<>(list));
             return;
         }
-        if(candidates[index]<=sum){
-            list.add(candidates[index]);
-            helper(candidates,index,sum-candidates[index],list,arr);
-            list.remove(list.size()-1);
-        }
+        if(sum<0 || index>=candidates.length) return;
+        list.add(candidates[index]);
+        helper(candidates,index,sum-candidates[index],list,arr);
+        list.remove(list.size()-1);
         helper(candidates,index+1,sum,list,arr);
     }
 }
