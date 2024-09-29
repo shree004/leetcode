53faf653624cd99517e348071ae764efd9856a87
Shree Varsaan <98572720+shree004@users.noreply.github.com>
2024-09-29 17:54:57 +0530

    Time: 1 ms (94.58%) | Memory: 44.3 MB (83.33%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,23 +1,29 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> arr = new ArrayList<>();
-        boolean[] freq = new boolean[nums.length];
-        helper(nums, new ArrayList(), arr, freq);
+        helper(0, nums, new ArrayList(), arr);
         return arr;
     }
-    public void helper(int[] nums,List<Integer> list, List<List<Integer>> arr,boolean[] freq){
-        if(list.size()==nums.length){
+
+    public void helper(int index, int[] nums, List<Integer> list, List<List<Integer>> arr) {
+        if (index == nums.length) {
+            for (int i = 0; i < nums.length; i++) {
+                list.add(nums[i]);
+            }
             arr.add(new ArrayList(list));
+            list.clear();
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!freq[i]){
-                freq[i]=true;
-                list.add(nums[i]);
-                helper(nums,list,arr,freq);
-                list.remove(list.size()-1);
-                freq[i]=false;
-            }
+        for (int i = index; i < nums.length; i++) {
+            swap(i, index, nums);
+            helper(index + 1, nums, list, arr);
+            swap(i, index, nums);
         }
+    }
+
+    public void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
