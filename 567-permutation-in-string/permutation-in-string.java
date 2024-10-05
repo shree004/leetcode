81c6db1d07eb10b3ca363d1b06602d5ff1b2ae3f
Shree Varsaan <98572720+shree004@users.noreply.github.com>
2024-10-05 22:17:29 +0530

    Time: 8 ms (60.46%) | Memory: 42.7 MB (53.57%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -2,25 +2,24 @@ public class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if (s1.length() > s2.length())
             return false;
-        HashMap<Character, Integer> s1map = new HashMap<>();
-
-        for (int i = 0; i < s1.length(); i++)
-            s1map.put(s1.charAt(i), s1map.getOrDefault(s1.charAt(i), 0) + 1);
-
-        for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            HashMap<Character, Integer> s2map = new HashMap<>();
-            for (int j = 0; j < s1.length(); j++) {
-                s2map.put(s2.charAt(i + j), s2map.getOrDefault(s2.charAt(i + j), 0) + 1);
-            }
-            if (matches(s1map, s2map))
-                return true;
+        int[] s1arr = new int[26];
+        int[] s2arr = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1arr[s1.charAt(i) - 'a']++;
+            s2arr[s2.charAt(i) - 'a']++;
         }
-        return false;
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1arr, s2arr))
+                return true;
+            s2arr[s2.charAt(i + s1.length()) - 'a']++;
+            s2arr[s2.charAt(i) - 'a']--;
+        }
+        return matches(s1arr, s2arr);
     }
-
-    public boolean matches(HashMap<Character, Integer> s1map, HashMap<Character, Integer> s2map) {
-        for (char key : s1map.keySet()) {
-            if (s1map.get(key) - s2map.getOrDefault(key, -1) != 0)
+    
+    public boolean matches(int[] s1arr, int[] s2arr) {
+        for (int i = 0; i < 26; i++) {
+            if (s1arr[i] != s2arr[i])
                 return false;
         }
         return true;
