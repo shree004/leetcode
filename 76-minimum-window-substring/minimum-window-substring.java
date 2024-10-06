8534e14fd0acd03e5c01d179d828c309172dd6ad
Shree Varsaan <98572720+shree004@users.noreply.github.com>
2024-10-06 23:23:22 +0530

    Time: 2 ms (99.91%) | Memory: 44.1 MB (98.71%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,29 +1,30 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length()<t.length() || s.length()==0 || t.length()==0) return "";
-        HashMap<Character,Integer> Tmap=new HashMap<>();
-        HashMap<Character,Integer> map=new HashMap<>();
+        char[] str=s.toCharArray();
+        int tmap[]=new int[128];
         for(int i=0;i<t.length();i++){
-            Tmap.put(t.charAt(i),Tmap.getOrDefault(t.charAt(i),0)+1);
+            tmap[t.charAt(i)]++;
         }
-        int ans[]={-1,0,0};
-        int l=0,count=0;
+        int required=t.length();
+        int start=0,min=Integer.MAX_VALUE;
+        int l=0;
         for(int r=0;r<s.length();r++){
-            char rchar=s.charAt(r);
-            map.put(rchar,map.getOrDefault(rchar,0)+1);
-            if(Tmap.containsKey(rchar) && map.get(rchar).intValue()==Tmap.get(rchar).intValue()) count++;
-            while(count==Tmap.size() && l<=r){
-                if(ans[0]==-1 || ans[0]>=r-l+1){
-                ans[0]=r-l+1;
-                ans[1]=l;
-                ans[2]=r;
+            if(tmap[str[r]]>0){
+                required--;
+            }
+            tmap[str[r]]--;
+            while(required==0){
+                if(r-l+1<min){
+                    min=r-l+1;
+                    start=l;
                 }
-                map.put(s.charAt(l),map.get(s.charAt(l))-1);
-                if(Tmap.containsKey(s.charAt(l)) && map.get(s.charAt(l)).intValue()<Tmap.get(s.charAt(l)).intValue()) count--;
+                if(tmap[str[l]]==0){
+                    required++;
+                }
+                tmap[str[l]]++;
                 l++;
             }
         }
-        if(ans[0]==-1)  return "";
-        return s.substring(ans[1],ans[2]+1);
+        return min==Integer.MAX_VALUE?"":new String(str,start,min);
     }
 }
